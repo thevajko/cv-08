177addd2bc89d510ee313f921d9890c4ca375882
Patrik Hrkút <Patrik.Hrkut@fri.uniza.sk>
2023-11-16 08:23:09 +0100

    Pridaný základ do štartéra

---
+++ js/modules/pesexo.js
@@ -0,0 +1,10 @@
+class Pexeso {
+
+    /**
+     * Array consists of icon to be used in the game. If you want mode cards in the game, add them here as values
+     */
+    static cardSymbols = ['yin-yang', 'tractor', 'truck-monster', 'truck', 'walking', 'ambulance', 'bicycle',
+        'bus', 'car', 'caravan', 'motorcycle', 'shuttle-van', 'tram', 'plane', 'subway'];
+
+
+}
