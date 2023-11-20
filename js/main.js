a7210299789ecd1411a589a45e24b29d3cc9f004
Matej Meško <meshosk@gmail.com>
2023-11-20 19:15:39 +0100

    ukazka modulu, priadna trieda player

---
+++ js/main.js
@@ -0,0 +1,4 @@
+import {Player} from "./modules/player.js";
+
+document.p2 = new Player();
+document.p1 = new Player();
--- js/modules/player.js
+++ js/modules/player.js
@@ -1,3 +1,11 @@
-class Player {
+export class Player {
+    #score = 0;
 
+    get score() {
+        return this.#score;
+    }
+
+    set score(value) {
+        this.#score = value;
+    }
 }
