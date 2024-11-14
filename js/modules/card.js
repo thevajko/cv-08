0de40ad8affcbf0833e0ffbc29b3586e77c30e60
hrkut <Patrik.Hrkut@fri.uniza.sk>
2024-11-14 12:53:47 +0100

    pexeso 3b

--- js/modules/card.js
+++ js/modules/card.js
@@ -1,3 +1,41 @@
 class Card {
+    #symbol
+    #guessed
+    #element
 
+    constructor(symbol) {
+        this.#symbol = symbol;
+    }
+
+    get symbol() {
+        return this.#symbol;
+    }
+
+    set symbol(value) {
+        this.#symbol = value;
+    }
+
+    get guessed() {
+        return this.#guessed;
+    }
+
+    set guessed(value) {
+        this.#guessed = value;
+    }
+
+    get element() {
+        return this.#element;
+    }
+
+    set element(value) {
+        this.#element = value;
+    }
+
+    show() {
+        this.#element.innerHTML = `<i class="fas fa-${this.#symbol} fa-3x" style="margin-top: 15px"></i>`;
+    }
+
+    hide() {
+        this.#element.innerHTML = ""
+    }
 }
--- js/modules/player.js
+++ js/modules/player.js
@@ -1,3 +1,25 @@
 class Player {
+    #number;
+    #score = 0;
+
+    constructor(number) {
+        this.#number = number;
+    }
+
+    get number() {
+        return this.#number;
+    }
+
+    set number(value) {
+        this.#number = value;
+    }
+
+    get score() {
+        return this.#score;
+    }
+
+    set score(value) {
+        this.#score = value;
+    }
 
 }
