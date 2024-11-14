98e46adb97a31c344573cd049108ca149a76ffa6
hrkut <Patrik.Hrkut@fri.uniza.sk>
2024-11-14 12:57:18 +0100

    pexeso 4.a

--- js/modules/pexeso.js
+++ js/modules/pexeso.js
@@ -3,8 +3,61 @@ class Pexeso {
     /**
      * Array consists of icon to be used in the game. If you want mode cards in the game, add them here as values
      */
-    static cardSymbols = ['yin-yang', 'tractor', 'truck-monster', 'truck', 'walking', 'ambulance', 'bicycle',
+    static _cardSymbols = ['yin-yang', 'tractor', 'truck-monster', 'truck', 'walking', 'ambulance', 'bicycle',
         'bus', 'car', 'caravan', 'motorcycle', 'shuttle-van', 'tram', 'plane', 'subway'];
 
+    #cardOne
+    #cardTwo
+    #cards
+    #players
+    #currentPlayer
 
+
+    static get cardSymbols() {
+        return this._cardSymbols;
+    }
+
+    static set cardSymbols(value) {
+        this._cardSymbols = value;
+    }
+
+    get cardOne() {
+        return this.#cardOne;
+    }
+
+    set cardOne(value) {
+        this.#cardOne = value;
+    }
+
+    get cardTwo() {
+        return this.#cardTwo;
+    }
+
+    set cardTwo(value) {
+        this.#cardTwo = value;
+    }
+
+    get cards() {
+        return this.#cards;
+    }
+
+    set cards(value) {
+        this.#cards = value;
+    }
+
+    get players() {
+        return this.#players;
+    }
+
+    set players(value) {
+        this.#players = value;
+    }
+
+    get currentPlayer() {
+        return this.#currentPlayer;
+    }
+
+    set currentPlayer(value) {
+        this.#currentPlayer = value;
+    }
 }
