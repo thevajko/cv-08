939223878178193817355e0f26ea0bbb6e161ce5
hrkut <Patrik.Hrkut@fri.uniza.sk>
2024-11-18 11:55:52 +0100

    Pexeso 3.2

--- js/modules/card.js
+++ js/modules/card.js
@@ -1,3 +1,43 @@
 class Card {
+    #symbol = ''
+    #element = null
+    #guessed = false
 
+    constructor(symbol) {
+        this.#symbol = symbol
+    }
+
+    get element() {
+        return this.#element;
+    }
+
+    set element(value) {
+        this.#element = value;
+    }
+
+    get guessed() {
+        return this.#guessed;
+    }
+
+    set guessed(value) {
+        this.#guessed = value;
+    }
+
+    get symbol() {
+        return this.#symbol;
+    }
+
+    set symbol(value) {
+        this.#symbol = value;
+    }
+
+    show() {
+        this.#element.innerHTML = `<i class="fas fa-${this.#symbol} fa-3x" style="margin-top: 15px"></i>`;
+    }
+
+    hide() {
+        this.#element.innerHTML = '';
+    }
 }
+
+export { Card }
