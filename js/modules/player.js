bad9c1c656be1c848bbbd54e85203a223bad0125
hrkut <Patrik.Hrkut@fri.uniza.sk>
2024-11-18 11:47:58 +0100

    Pexeso 2

--- js/modules/player.js
+++ js/modules/player.js
@@ -1,3 +1,26 @@
 class Player {
+    #number
+    #score = 0
 
+    constructor(number) {
+        this.#number = number;
+    }
+
+    get number() {
+        return this.#number;
+    }
+
+    set number(value) {
+        this.#number = value;
+    }
+
+    get score() {
+        return this.#score;
+    }
+
+    set score(value) {
+        this.#score = value;
+    }
 }
+
+export { Player };
