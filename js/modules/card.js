c33bf656389ffc306680005502913cc7f42bec5b
hrkut <hrkut@kst.uniza.sk>
2024-11-18 08:53:53 +0100

    Pexeso 3a

--- js/modules/card.js
+++ js/modules/card.js
@@ -1,3 +1,35 @@
 class Card {
+    #guessed = false;
+    #symbol
+    #element
 
+    constructor(symbol) {
+        this.#symbol = symbol;
+    }
+
+    get symbol() {
+        return this.#symbol;
+    }
+
+    set symbol(value) {
+        this.#symbol = value;
+    }
+
+    get element() {
+        return this.#element;
+    }
+
+    set element(value) {
+        this.#element = value;
+    }
+
+    get guessed() {
+        return this.#guessed;
+    }
+
+    set guessed(value) {
+        this.#guessed = value;
+    }
 }
+
+export { Card };
--- js/modules/player.js
+++ js/modules/player.js
@@ -1,3 +1,23 @@
 class Player {
+    #number
+    #score = 0
 
+
+    get score() {
+        return this.#score;
+    }
+
+    set score(value) {
+        this.#score = value;
+    }
+
+    get number() {
+        return this.#number;
+    }
+
+    set number(value) {
+        this.#number = value;
+    }
 }
+
+export { Player };
