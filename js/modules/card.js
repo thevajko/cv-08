6aa84e7587754bcadf4906391610f95e6339b096
mesko <meshosk@gmail.com>
2025-11-19 11:42:38 +0100

    Pridanie triedy Card s privátnymi vlastnosťami a getter/setter metódami

--- js/modules/card.js
+++ js/modules/card.js
@@ -1,3 +1,37 @@
 class Card {
+    #symbol;
+    #element;
+    #guessed;
 
+    constructor(symbol) {
+        this.#symbol = symbol;
+        this.#guessed = false;
+    }
+
+
+    get element() {
+        return this.#element;
+    }
+
+    set element(value) {
+        this.#element = value;
+    }
+
+    get symbol() {
+        return this.#symbol;
+    }
+
+    set symbol(value) {
+        this.#symbol = value;
+    }
+
+    get guessed() {
+        return this.#guessed;
+    }
+
+    set guessed(value) {
+        this.#guessed = value;
+    }
 }
+
+export default Card;
